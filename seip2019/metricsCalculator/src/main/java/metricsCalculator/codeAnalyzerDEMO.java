c85175a620d0e8783c6dbcfa08a9e631787a14cf
Nikos Fragkomanolis <nickosfra@gmail.com>
2019-04-03 00:45:35 +0300

    Fill main method with appropriate facade object to analyze the file

--- seip2019/metricsCalculator/src/main/java/metricsCalculator/codeAnalyzerDEMO.java
+++ seip2019/metricsCalculator/src/main/java/metricsCalculator/codeAnalyzerDEMO.java
@@ -3,8 +3,14 @@ package metricsCalculator;
 public class codeAnalyzerDEMO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		if (args.length == 0) {
+			System.err.println("Exactly two arguments required: [sourceFile] [string or regex]");
+			System.exit(1);
+		}
+		
+		codeAnalyzer analyzer = new codeAnalyzer();
+		analyzer.analyzeCode(args[0], args[1], "./metrics.csv");
 	}
 
 }
