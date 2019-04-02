2d4e94e0f9b6109003e89dfbeed3bd9c8ac233e6
Nikos Fragkomanolis <nickosfra@gmail.com>
2019-04-03 00:38:20 +0300

    Add analyzeCode and combineMetrics methods

--- seip2019/metricsCalculator/src/main/java/metricsCalculator/codeAnalyzer.java
+++ seip2019/metricsCalculator/src/main/java/metricsCalculator/codeAnalyzer.java
@@ -1,15 +1,41 @@
 package metricsCalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class codeAnalyzer {
 	
-	public codeAnalyzer() {
+	public void analyzeCode(String pathOfSourceFile, String typeOfAnalysis, String outputFile) {		
+		int linesOfCode;
+		int numberOfClasses;
+		int numberOfMethods;
+		List<String> metrics = new ArrayList<String>();
 		
+		List<String> lines = new ArrayList<String>();
+		try {
+			lines = fileIOUtilities.readFile(pathOfSourceFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Analyzer analyzer = AnalyzerFactory.createAnalyzer(typeOfAnalysis);
+		linesOfCode = analyzer.countLines(lines);
+		numberOfClasses = analyzer.countClasses(lines);
+		numberOfMethods = analyzer.countMethods(lines);
+		metrics = codeAnalyzer.combineMetrics(linesOfCode, numberOfClasses, numberOfMethods);		
+		
+		fileIOUtilities.writeFile(outputFile, metrics);
 	}
 	
-	public static void analyzeCode(String pathOfSourceFile, String typeOfAnalysis, String outputFile) {
+	public static List<String> combineMetrics(int linesOfCode, int numberOfClasses, int numberOfMethods){
+		List<String> list = new ArrayList<String>();
+		list.add(String.valueOf(linesOfCode));
+		list.add(",");
+		list.add(String.valueOf(numberOfClasses));
+		list.add(",");
+		list.add(String.valueOf(numberOfMethods));		
 		
+		return list;		
 	}
-	
-	
 
 }
