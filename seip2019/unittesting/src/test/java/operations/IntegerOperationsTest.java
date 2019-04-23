4103fa9d44f39fd4e861733017ee955b5b0e29a3
Nikos Fragkomanolis <nickosfra@gmail.com>
2019-04-24 02:10:53 +0300

    Test for IntegerOperations class

---
+++ seip2019/unittesting/src/test/java/operations/IntegerOperationsTest.java
@@ -0,0 +1,52 @@
+package operations;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegerOperationsTest {
+	
+	IntegerOperations iop = new IntegerOperations();
+	
+	@Test
+	public void test_getSign_positive() {
+		Assert.assertEquals(-3, iop.reverseSign(3));
+	}
+	
+	
+	@Test
+	public void test_getSign_negative() {
+		Assert.assertEquals(3, iop.reverseSign(-3));
+	}
+	
+	
+	@Test
+	public void test_getSign_zero() {
+		Assert.assertEquals(0, iop.reverseSign(0));
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getNegativeNumber_exception() {
+		iop.add(2,-1);
+		iop.add(-2,1);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getMaxNumber_exception() {
+		iop.add(Integer.MAX_VALUE - 5, 8);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class) 
+	public void test_getNegativeInteger_exception() {
+		iop.powerOfTwo(-5);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getBigInteger_exception() {
+		iop.powerOfTwo(36);
+	}
+
+}
