9676e0f1f2e52730eff608227bbf266678386106
Nikos Fragkomanolis <nickosfra@gmail.com>
2019-04-24 02:11:43 +0300

    Test suite

---
+++ seip2019/unittesting/src/test/java/operations/IntegerOperationsTestSuite.java
@@ -0,0 +1,11 @@
+package operations;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({IntegerOperationsTest.class, IntegerOperations_powerOfTwo_ParameterizedTest.class})
+public class IntegerOperationsTestSuite {
+	
+}
